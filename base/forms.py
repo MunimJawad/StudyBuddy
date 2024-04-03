285b862ab5fc708bc4c22b6067d0f3ab4f9bfdfd
Obayed <mnm.jwd01@gmail.com>
2024-04-03 16:19:34 +0600

    first commit

---
+++ base/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from .models import Room, User
+from django.contrib.auth.forms import UserCreationForm
+
+class RoomForm(ModelForm):
+    class Meta:
+        model=Room
+        fields='__all__'
+        exclude = ['host', 'participants']
+
+class userForm(ModelForm):
+    class Meta:
+        model=User
+        fields = ['avatar','name','username', 'email', 'bio']
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['name','username','email','password1','password2','avatar']
